0925d7e8a43cc27f823b6d1bafbeb0a28056e8a9
erine <erine@iastate.edu>
2018-02-11 15:38:33 -0600

    uploaded flaskpractice files, can upload photo

---
+++ B2/FlaskPractice/hello.py
@@ -0,0 +1,41 @@
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from flask import render_template
+
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+# @app.route('/hello')
+# def hello():
+#     return 'Hello, World'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['the_file']
+        f.save('/var/www/uploads/uploaded_file.txt')
