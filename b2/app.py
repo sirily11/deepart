c28ebc5c36d46b39cda45b371c3d45407ac066c1
liqiwei <liqiwei@iastate.edu>
2018-03-23 09:08:57 -0500

    added news

--- b2/app.py
+++ b2/app.py
@@ -1,40 +1,31 @@
-import json
 import os
-
-from PIL import Image
-from celery import Celery
 from flask import Flask, request, redirect, url_for, flash
 from flask import render_template
 from werkzeug.utils import secure_filename
-from models.deepart import style_transform
-from models.firebase import login_user_with_eamil
-from models.firebase import signup_user_with_email
+import json
+from PIL import Image
+from deepart import style_transform
 
 SYSTEM_FOLDER = os.getcwd()
 UPLOAD_FOLDER = 'uploaded/images/'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = SYSTEM_FOLDER + "/static/" + UPLOAD_FOLDER
-# Celery configuration
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
-celery = Celery('myapp')
-celery.conf.add_defaults(app.config)
-
 img_size = [300, 300]
-selected_styles = []
 
 '''
 Main page
 '''
-@app.route('/home')
-def home():
-    return render_template("home_page.html")
+@app.route('/')
+def main():
+    return render_template("hello.html")
 
 
 '''
-data for ComS 309
+data for ComS 319
 '''
+
+
 @app.route('/data/', methods=['GET'])
 def data():
     account = request.args.get('account')
@@ -47,19 +38,11 @@ def data():
                {"account_id": account, "first_name": "Json", "last_name": "Hacky", "img_url": img[2]}]
     return json.dumps(sumdict)
 
+
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
         # check if the post request has the file part
-
-        if "start_generate" in request.form:
-            print("Start")
-
-            style_transform.apply_async(('static/uploaded/generate_images',
-                        "static/" + view_all_images()[0],
-                        "static/" + view_all_style_images()[0]))
-            return "start processing"
-
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
@@ -77,97 +60,31 @@ def upload():
             img = Image.open(savePath)
             img.thumbnail(img_size)
             img.save(savePath)
-            imglist = view_all_images()
-            return render_template("picster_styles.html", has_uploaded=True,
-                                   user_image=UPLOAD_FOLDER + filename,
-                                   images=imglist,style_images = view_all_style_images())
-    return render_template("picster_styles.html", has_uploaded=False,
-                           style_images = view_all_style_images(),selected_style_imgs=selected_styles)
+            return render_template("upload_file.html", has_uploaded=True, user_image=UPLOAD_FOLDER + filename)
 
-@app.route('/faq')
-def faq():
-    return render_template('FAQ.html')
-
-@app.route('/signup')
-def signup():
-    if request.method == 'POST':
-        email = request.form['login_email']
-        password = request.form['login_password']
-        message = signup_user_with_email(email,password)
-        print(message)
-        if message != False:
-            return redirect("/home")
-        else:
-            return redirect("/home")
-    return render_template('login.html',message="Hello world")
-
-@app.route('/careers')
-def careers():
-    return render_template('careers.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-@app.route('/gallery')
-def gallery():
-    return render_template('gallery.html', transform_images=view_all_transform_images())
+    return render_template("upload_file.html", has_uploaded=False)
 
 
-@app.route('/login',methods=['POST','GET'])
-def login():
-    if request.method == 'POST':
-        email = request.form['login_email']
-        password = request.form['login_password']
-        message = login_user_with_eamil(email,password)
-        print(message)
-        if message != False:
-            return redirect("/home")
-        else:
-            return redirect("/login")
-    return render_template("Login.html")
-
-
+@app.route('/view_all_images')
 def view_all_images():
     imgList = os.listdir(app.config['UPLOAD_FOLDER'])
     newImgList = []
     for img in imgList:
         if allowed_file(img):
             newImgList.append(UPLOAD_FOLDER + img)
-    return newImgList
+    return render_template("view_all_images.html", images=newImgList)
 
-def view_all_style_images():
-    imgList = os.listdir('static/uploaded/style_images/')
-    newImgList = []
-    for img in imgList:
-        if allowed_file(img):
-            newImgList.append('uploaded/style_images/' + img)
-    return newImgList
+
+@app.route('/picster_styles')
+def select_styles():
+    return render_template("picster_styles.html")
+
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def make_celery(app):
-    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
-    class ContextTask(TaskBase):
-        abstract = True
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-    celery.Task = ContextTask
-    return celery
-def view_all_transform_images():
-    imgList = os.listdir('static/uploaded/transform_images/')
-    newImgList = []
-    for img in imgList:
-        if allowed_file(img):
-            newImgList.append('uploaded/transform_images/' + img)
-    return newImgList
 
 if __name__ == '__main__':
-
     app.run(host='0.0.0.0', port=80, debug=True)
-    celery = make_celery(app)
